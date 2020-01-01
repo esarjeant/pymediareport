5c7b38c9d6f24b8fb9c44dcbb79a5bcf00f5e627
Eric Sarjeant <eric@sarjeant.us>
2020-01-01 14:58:35 -0500

    flag for verbose mode; otherwise just filename

--- pymediareport.py
+++ pymediareport.py
@@ -50,6 +50,7 @@ parser.add_argument('path', metavar='directory', help='directory to check video 
 parser.add_argument('--extension', dest='extension', action='store', default="m4v", help='extension to search; common ones include m4v and mpg')
 parser.add_argument('--min_bit_rate', dest='min_bit_rate', action='store', default="500000", type=int, help='minimum bitrate for --find_low_bit_rate')
 parser.add_argument('--recursive', dest='recursive', action='store_true', help='allow recursion into all sub-directories')
+parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose output which includes media details')
 parser.add_argument('--find_stereo_only', dest='find_stereo_only', action='store_true', help='filter results that have stereo only')
 parser.add_argument('--find_single_audio_only', dest='find_single_audio_only', action='store_true', help='filter results that have a single audio stream')
 parser.add_argument('--find_fullscreen', dest='find_fullscreen', action='store_true', help='filter results that are fullscreen aspect ratio')
@@ -82,4 +83,7 @@ for filename in glob.iglob(args.path + file_mask, recursive=args.recursive):
         pass_filter = is_low_bit_rate(metadata, args.min_bit_rate)
 
     if pass_filter:
-        print(filename+" -> "+str(metadata))
+        if args.verbose:
+            print(filename+" -> "+str(metadata))
+        else:
+            print(filename)
